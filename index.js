5c6fda455bdd944af0d5ffae1443d3cfe13ad2df
raftouch <rafanelkin@gmail.com>
2026-06-22 21:22:15 +0200

    configure dotenv and start working with sockets

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const WSServer = require("express-ws")(app);
+
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.ws("/", (ws, req) => {
+  console.log("All works");
+  ws.on("message", (msg) => {
+    console.log(msg);
+  });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
