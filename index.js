84d9f096bfe1090b4473905f8bdc083724598e98
raftouch <rafanelkin@gmail.com>
2026-07-10 14:20:47 +0200

    feat: add download image logic

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const WSServer = require("express-ws")(app);
 const aWss = WSServer.getWss();
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
 
 require("dotenv").config();
 const port = process.env.PORT;
@@ -42,5 +48,22 @@ const broadcastConnection = (ws, msg) => {
     }
   });
 };
+
+app.post("/image", (req, res) => {
+  try {
+    const data = req.body.img.replace("data:image/png;base64,", "");
+
+    fs.writeFileSync(
+      path.resolve(__dirname, "files", `${req.query.id}.png`),
+      data,
+      "base64",
+    );
+
+    return res.status(200).json({ message: "Downloaded" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Something went wrong");
+  }
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
